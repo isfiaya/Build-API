8943869ad42b1d946e72301f9bf1f1a780ea4d05
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-03-03 01:57:54 +0100

    perf: Optimize the structure

--- routes/sauces .js
+++ routes/sauces .js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const sauce = require('../models/Sauces');
-const jwt = require('jsonwebtoken');
 const sauceCtrl = require('../controllers/sauce');
 
 
