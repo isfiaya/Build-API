6bf9b0a961f0eb6cfad3c6771b906565c8139b65
aymen isfiaya <aymen.isfiaya@gmail.com>
2021-03-11 02:04:58 +0100

    add: Added the likeSauce function

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -72,7 +72,28 @@ exports.submitSauce = async (req, res) => {
   }
 }
 
-exports.userlikes = async (req, res) => {
-  const action = req.body.action;
-  const counter = action === 'like' ? 1 : -1;
+exports.likeSauce = async (req, res) => {
+  try {
+    const oneSauce = await sauce.findById(req.params.id);
+    if (req.body.like === 1) {
+      oneSauce.usersLiked.push(req.body.userId);
+      oneSauce.likes += 1;
+    }
+    else if (req.body.like === 0 && oneSauce.usersLiked.includes(req.body.userId)) {
+      oneSauce.usersLiked.remove(req.body.userId);
+      oneSauce.likes += -1;
+    }
+    else if (req.body.like === -1) {
+      oneSauce.usersDisliked.push(req.body.userId);
+      oneSauce.dislikes += 1;
+    }
+    else if (req.body.like === 0 && oneSauce.usersDisliked.includes(req.body.userId)) {
+      oneSauce.usersDisliked.remove(req.body.userId);
+      oneSauce.dislikes += -1;
+    }
+    oneSauce.save()
+    res.status(201).json({ message: "Preference sended to the system." })
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
 }
